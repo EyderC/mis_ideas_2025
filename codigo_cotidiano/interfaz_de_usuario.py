b40ba5691c0fa57bd027cdfcd7d2849e4139a232
EyderC <eyderc_27@hotmail.es>
2026-01-09 19:03:37 -0500

    Escribir encabezado en archivo comidas.csv

--- codigo_cotidiano/interfaz_de_usuario.py
+++ codigo_cotidiano/interfaz_de_usuario.py
@@ -1,12 +1,26 @@
+import csv
+
 
 print('\nnombres de columnas de la tabla de datos:'.upper())
-nombre_columnas = []
+encabezado = []
 columna = input('\t\t- ')
 while(columna != 'salir'):
-    if columna != 'salir' and columna != '':
-        nombre_columnas.append(columna)
+    if columna != 'salir' and columna != '': #Para que estas dos opciones no se guarden como encabezados 
+        encabezado.append(columna)
 
     columna = input('\t\t- ')
-    
 
-print(nombre_columnas)
+nombre_archivo = input('nombre del archivo para guardar datos: '.title())
+nombre_archivo.rstrip()
+nombre_archivo.lstrip()
+nombre_archivo.lower()
+
+with open(nombre_archivo,'w', newline='', encoding='utf-8') as archivo_csv:
+    escribir = csv.writer(archivo_csv)
+    escribir.writerow(encabezado)
+
+
+
+
+
+
